3224748a2b832bd53da2db96afb21c298f523e24
Dennis Bystrow <db@quality-match.com>
2020-04-08 17:16:18 +0200

    initial commit

---
+++ Assets/Scenes/MyAwesomeTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAwesomeTrigger : MonoBehaviour
+{
+    private bool _isTasksSdkWebviewActive;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !_isTasksSdkWebviewActive)
+        {
+            _isTasksSdkWebviewActive = true;
+            QualityMatch.Tasks.Sdk.ShowTask((status, result) => {
+                _isTasksSdkWebviewActive = false;
+                Debug.Log(string.Format("OnTaskCompleted: [{0}] {1}", status, JsonUtility.ToJson(result)));
+            });
+        }
+    }
+}
