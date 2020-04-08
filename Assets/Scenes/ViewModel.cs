a4120289ec39c91a023b7227975886aa64ca9766
Dennis Bystrow <db@quality-match.com>
2020-04-08 17:49:45 +0200

    added button to trigger loading of task-ui

---
+++ Assets/Scenes/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewModel : MonoBehaviour
+{
+    public void Load_Task_UI()
+    {
+        QualityMatch.Tasks.Sdk.ShowTask((status, result) => {
+            Debug.Log(string.Format("OnTaskCompleted: [{0}] {1}", status, JsonUtility.ToJson(result)));
+        });
+    }
+}
